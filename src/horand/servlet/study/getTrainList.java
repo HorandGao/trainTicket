3eae4759174320edfd4319497830ad3b32f6d2a3
Administrator <horand_gc@163.com>
2016-04-01 20:49:14 +0800

    advance

--- src/horand/servlet/study/getTrainList.java
+++ src/horand/servlet/study/getTrainList.java
@@ -2,10 +2,10 @@ package horand.servlet.study;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,17 +15,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
 /**
- * Servlet implementation class addUser
+ * Servlet implementation class getTrainList
  */
-@WebServlet("/addUser.action")
-public class addUser extends HttpServlet {
+@WebServlet("/getTrainList.action")
+public class getTrainList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public addUser() {
+    public getTrainList() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,41 +38,44 @@ public class addUser extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  request.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
 		  response.setCharacterEncoding("utf-8");
 		  response.setContentType("application/json; charset=utf-8");
+		  //response.setContentType("text/html; charset=utf-8");
 		  PrintWriter out = response.getWriter();
 	      Statement stmt = null;
-	      int rs = 100;
+	      ResultSet rs = null;
 
 	      try {
 	            Class.forName("com.mysql.jdbc.Driver");
 	            Connection conn = DriverManager.getConnection("jdbc:mysql://115.28.158.46:3306/train?user=root&password=horand&useUnicode=true&characterEncoding=utf8");
 	           
 	            stmt = conn.createStatement();
-	            String reqName = request.getParameter("name");
-	            String reqPwd = request.getParameter("pwd");
-	            String reqIDcard = request.getParameter("idcard");
-	            String reqPhone = request.getParameter("phone");
-	            String reqRealName = request.getParameter("realName");
+	            String date = request.getParameter("date");
+	            String src = request.getParameter("src");
+	            String des = request.getParameter("des");
 	            
 	            if(request.getMethod().equalsIgnoreCase("GET"))
 	            {
-	            	reqRealName = new String(reqRealName.getBytes("iso8859-1"),"utf-8");
+	            	src = new String(src.getBytes("iso8859-1"),"utf-8");
+	            	des = new String(des.getBytes("iso8859-1"),"utf-8");
 	            }
-	            if(reqName==null || reqPwd==null || reqIDcard==null || reqPhone==null || reqRealName==null ||
-	            		reqName=="" || reqPwd=="" || reqIDcard=="" || reqPhone=="" || reqRealName==""){
-	            	out.write("{\"success\":\"0\"}");
-	            	return;
+
+	            //String str_temp = "select * from trainInfo join ticketInfo on train_num=trainNum where date='2016-04-01'";
+	            String str_sql = "select * from trainInfo join ticketInfo on train_num=trainNum  where train_src='"+src+"' and train_des='"+des+"' and date='"+date+"'";
+	            rs = stmt.executeQuery(str_sql);
+	            
+	            out.write(resultSetToJson(rs));
+	            //out.println(date+src+des);
+	           // out.println(str_sql);
+	            
+	            if (rs != null) {
+	                try {
+	                    rs.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
 	            }
-	            rs = stmt.executeUpdate("insert into user values(NULL,'"+reqName+"','"+reqPwd+"','"+reqIDcard+"',"+reqPhone+",'"+reqRealName+"')");
-	            if(rs>=1){
-	            	out.write("{\"success\":\"1\"}");
-	            }
-	            else{
-	            	out.write("{\"success\":\"0\"}");
-	            }
-	            out.write(rs);
 	            if (stmt != null) {
 	                try {
 	                	stmt.close();
@@ -97,5 +104,26 @@ public class addUser extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	public String resultSetToJson(ResultSet rs) throws SQLException,JSONException  
+    {  
+
+       JSONArray array = new JSONArray();  
+        
+       ResultSetMetaData metaData = rs.getMetaData();  
+       int columnCount = metaData.getColumnCount();  
+        while (rs.next()) {  
+            JSONObject jsonObj = new JSONObject();  
+             
+            for (int i = 1; i <= columnCount; i++) {  
+                String columnName =metaData.getColumnLabel(i);  
+                String value = rs.getString(columnName);  
+                jsonObj.put(columnName, value);  
+            }   
+            array.put(jsonObj);   
+        }  
+        
+       return array.toString();  
+    }  
 
 }
