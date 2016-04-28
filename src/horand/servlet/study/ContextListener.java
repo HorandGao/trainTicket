729c4b3398b5ae808bb14789bf47e62f0a6d56b2
GAOCHUN003 <GAOCHUN003@O70512031201306.htinns.loc>
2016-04-28 15:49:58 +0800

    定时器 乱码

--- src/horand/servlet/study/ContextListener.java
+++ src/horand/servlet/study/ContextListener.java
@@ -9,16 +9,10 @@ public class ContextListener implements ServletContextListener {
       
     public void contextInitialized(ServletContextEvent event) {  
         timer = new java.util.Timer(true);  
-        event.getServletContext().log("定时器已启动");  
-        //System.out.println("*******************定时器已启动");  
         timer.schedule(new MyTask(event.getServletContext()), 0, 30*1000);  
-        //System.out.println("********************已经添加任务调度表");  
-        event.getServletContext().log("已经添加任务调度表");  
     }  
     public void contextDestroyed(ServletContextEvent event) {  
         timer.cancel();  
-        //System.out.println("*************定时器销毁");  
-        event.getServletContext().log("定时器销毁");  
     }  
 }  
 class Time{  
